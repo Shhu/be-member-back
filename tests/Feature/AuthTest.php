453d762b7699c334851a103cdcf074015cc8c510
Yann Aubriot <lowscaa@gmail.com>
2020-02-13 13:41:56 +0100

    Test database

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -2,17 +2,36 @@
 
 namespace Tests\Feature;
 
+use Bemember\User\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
+    use DatabaseMigrations;
+    use RefreshDatabase;
 
     /** @test */
     public function unauthenticated_return_a_401_with_unauthenticated_message(): void
     {
         $reponse = $this->get('/api/auth/unauthenticated');
         $reponse->assertStatus(401)->assertSee('Unauthenticated');
+    }
+
+    /** @test */
+    public function invalid_credentials_must_return_a_json(): void
+    {
+        $reponse = $this->post('/api/auth/login');
+        $reponse->assertStatus(422)->assertJsonStructure();
+    }
+
+    /** @test */
+    public function successfully_authenticated_return_a_token(): void
+    {
+        $user = \factory(User::class)->create();
+        $response = $this->post('/api/auth/login', ['email' => $user->email, 'password' => 'password']);
+        $response->assertStatus(200)->assertJsonStructure(['token']);
     }
 
 }
--- tests/Feature/FrontTest.php
+++ tests/Feature/FrontTest.php
@@ -11,9 +11,8 @@ class FrontTest extends TestCase
     /** @test */
     public function front_return_a_200_with_default_message(): void
     {
-        $this->get('/')
-            ->assertStatus(200)
-            ->assertSee('Bemember API');
+        $reponse = $this->get('/');
+        $reponse->assertStatus(200)->assertSee('Bemember API');
     }
 
 }
