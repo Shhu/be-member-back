6831e97f2795c46c8a13ed109687cf77dcf4ce86
Yann Aubriot <lowscaa@gmail.com>
2020-02-16 09:34:07 +0100

    docblock

--- Bemember/User/Controllers/Edit.php
+++ Bemember/User/Controllers/Edit.php
@@ -9,6 +9,12 @@ use Bemember\User\Resources\EditResource;
 
 class Edit extends Controller
 {
+    /**
+     * @param Request $request
+     * @param User $user
+     * @return EditResource
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
     public function __invoke(Request $request, User $user)
     {
         $this->authorize('edit', $user);
